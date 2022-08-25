45392deb12902f65ead9e23ba681c15729db20f6
Kai Chan <owenkaic@gmail.com>
2022-08-25 23:11:32 +0100

    11 tests working but can't get code to ignore text

---
+++ src/main/java/ParenthesesChecker.java
@@ -0,0 +1,46 @@
+import java.util.Stack;
+
+public class ParenthesesChecker {
+
+// Create a stack that holds the characters we want
+// create a for loop that looks for the parentheses
+// if the character meets our condition, put it in the stack
+// compare the next character to see if it matches what we are looking for
+//    if it does, pop it out of stack [essentially remove it from stack]
+//    Need to figure out how to ignore alphabet characters
+public boolean checkParentheses(String input) {
+
+    // Create a stack named container to hold characters we want
+    Stack<Character> container = new Stack<Character>();
+
+    char[] chars = input.toCharArray();
+
+
+
+        for (char current : chars) {
+            if (current == '(' || current == '{' || current == '[') {
+                container.push(current);
+
+            } else {
+                if (container.isEmpty()) {
+                    return false;
+                } else if ((current == ')' && container.peek() != '('
+                        || (current == '}' && container.peek() != '{')
+                        || (current == ']' && container.peek() != '['))) {
+                    return false;
+
+                } else {
+                    container.pop();
+
+                }
+
+            }
+
+        }
+        return container.isEmpty();
+
+    }
+}
+
+
+
